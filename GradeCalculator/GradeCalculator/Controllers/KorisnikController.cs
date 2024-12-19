ae7df5daf85450b22c4beacedbc96e6a329f27ce
Mihovil Jurela <jurela.mihovil@gmail.com>
2024-12-19 14:02:59 +0100

    User detail and edit

--- GradeCalculator/GradeCalculator/Controllers/KorisnikController.cs
+++ GradeCalculator/GradeCalculator/Controllers/KorisnikController.cs
@@ -1,21 +1,57 @@
-﻿using GradeCalculator.Security;
+﻿using GradeCalculator.Models;
+using GradeCalculator.Security;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
+using System;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using GradeCalculator.ViewModels;
+using NuGet.Protocol;
+using System.Diagnostics;
 
 namespace GradeCalculator.Controllers
 {
     public class KorisnikController : Controller
     {
+        private readonly PiGradeCalculatorContext _context;
+
+        public KorisnikController(PiGradeCalculatorContext context)
+        {
+            _context = context;
+        }
+
+        private const int REGULAR_USER_ID = 1;
+
         // GET: KorisnikController
         public ActionResult Index()
         {
             return View();
         }
 
-        // GET: KorisnikController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var user = _context.Korisniks
+                .Include(r => r.Uloga)
+                .FirstOrDefault(r => r.Idkorisnik == id);
+            if (user == null)
+            {
+                return NotFound($"Could not find user with id {id}");
+            }
+
+            var userVm = new KorisnikVM
+            {
+                Id = user.Idkorisnik,
+                UserName = user.KorisnickoIme,
+                Email = user.Eposta,
+                TotalGrade = user.UkupnaOcjena,
+                RoleId = user.UlogaId
+            };
+
+            return View(userVm);
         }
 
         // GET: KorisnikController/Create
@@ -27,10 +63,46 @@ namespace GradeCalculator.Controllers
         // POST: KorisnikController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(KorisnikVM userVm)
         {
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+                    {
+                        Console.WriteLine(error.ErrorMessage);
+                    }
+                    return View();
+                }
+
+                if (_context.Korisniks.Any(x => x.KorisnickoIme.Equals(userVm.UserName)))
+                {
+                    ModelState.AddModelError("UserName", "User already EXISTS");
+                    return View();
+                }
+
+                if (_context.Korisniks.Any(x => x.Eposta.Equals(userVm.Email)))
+                {
+                    ModelState.AddModelError("Email", "Email already EXISTS");
+                    return View();
+                }
+
+                var b64salt = PasswordProvider.GetSalt();
+                var b64hash = PasswordProvider.GetHash(userVm.Password, b64salt);
+
+                var user = new Korisnik
+                {
+                    KorisnickoIme = userVm.UserName,
+                    Eposta = userVm.Email,
+                    LozinkaHash = b64hash,
+                    LozinkaSalt = b64salt,
+                    UlogaId = REGULAR_USER_ID
+                };
+
+                _context.Add(user);
+                _context.SaveChanges();
+
                 return RedirectToAction(nameof(Index));
             }
             catch
