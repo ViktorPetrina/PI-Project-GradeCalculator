cfb827eca8c34ce4e3543c58bfb547d1cfe0d179
mico <mislav.vukusicbusinesss@gmail.com>
2025-01-04 17:54:12 +0100

    Dodan UserLoginController, navigacije do prijave
    Navigacijski botun ne radi tocno

---
+++ GradeCalculator/GradeCalculator/Controllers/UserLoginController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace GradeCalculator.Controllers
+{
+    public class UserLoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login(string returnUrl)
+        {
+            var claims = new List<Claim>();
+
+            var claimsIdentity = new ClaimsIdentity(
+                claims,
+                CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties();
+
+            // We need to wrap async code here into synchronous since we don't use async methods
+            Task.Run(async () =>
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity),
+                    authProperties)
+            ).GetAwaiter().GetResult();
+
+            if (returnUrl != null)
+                return LocalRedirect(returnUrl);
+            else
+                return  RedirectToAction("Index", "Home");
+        }
+    }
+}
