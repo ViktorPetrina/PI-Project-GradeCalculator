c02db59c6ef62b45f9cd50ef0e6bf1bbe4b0582c
mico <mislav.vukusicbusinesss@gmail.com>
2025-01-07 00:27:51 +0100

    Solidi smh

---
+++ GradeCalculator/GradeCalculator/Service/IUkupnaStatistika.cs
@@ -0,0 +1,8 @@
+﻿namespace GradeCalculator.Service
+{
+    public interface IUkupnaStatistika
+    {
+        //Kalkulira ukupni prosjek svih prosjeka korisnika
+        Dictionary<int, double> KalkulacijaUkupnihProsjeka();
+    }
+}
